a2da0d7bc8422913d3ffe56a2193c0c7d55eb2ba
PS001 <luigi.zielone@gmail.com>
2018-04-25 21:57:32 +0200

    lab3 - graphs optimized 3

--- lab3/README.md
+++ lab3/README.md
@@ -6,7 +6,8 @@
 Opis zastosowanych optymalizacji:
 1. zmienne globalne `N, K` jako stałe,
 2. reprezentacja macierzy sąsiedztwa grafu jako `BitArray`,
-3.
+3. zmienne opisujące graf przekazywane jako argumenty do funkcji,
+  `nodes` jako `Array{NodeType, 1}`, `graph` jako `Array{GraphVertex, 1}`,
 4.
 5.
 
@@ -15,6 +16,6 @@ Opis zastosowanych optymalizacji:
 |      0  | 12.871667 seconds | (121.82 M allocations: 6.527 GiB, 13.89% gc time) |
 |      1  |  4.177649 seconds | (5.49 M allocations: 3.790 GiB, 16.52% gc time)|  |
 |      2  |  3.526876 seconds | (5.50 M allocations: 3.321 GiB, 18.64% gc time)|  |
-|      3  |  |  |
+|      3  |  3.134581 seconds | (3.78 M allocations: 3.288 GiB, 19.86% gc time)|  |
 |      4  |  |  |
 |      5  |  |  |
--- lab3/graphs.jl
+++ lab3/graphs.jl
@@ -27,17 +27,9 @@ mutable struct Address <: NodeType
   streetNumber
 end
 
-
-# Number of graph nodes.
-const N = 800
-
-# Number of graph edges.
-const K = 10000
-
-
 #= Generates random directed graph of size N with K edges
 and returns its adjacency matrix.=#
-function generate_random_graph()
+function generate_random_graph(N, K)
     A = BitArray(N, N)
     A .= false
 
@@ -60,10 +52,14 @@ function get_random_address()
 end
 
 # Generates N random nodes (of random NodeType).
-function generate_random_nodes()
-  nodes = Vector()
-  for i= 1:N
-    push!(nodes, rand() > 0.5 ? get_random_person() : get_random_address())
+function generate_random_nodes(N)
+  nodes = Array{NodeType, 1}(N)
+  for i = 1:N
+    if (rand() > 0.5)
+      nodes[i] = get_random_person()
+    else
+      nodes[i] = get_random_address()
+    end
   end
   nodes
 end
@@ -71,19 +67,20 @@ end
 #= Converts given adjacency matrix (NxN)
   into list of graph vertices (of type GraphVertex and length N). =#
 function convert_to_graph(A, nodes)
-  N = length(nodes)
-  push!(graph, map(n -> GraphVertex(n, GraphVertex[]), nodes)...)
+  B = length(nodes)
+  graph = map(n -> GraphVertex(n, GraphVertex[]), nodes)
 
-  for i = 1:N, j = 1:N
+  for i = 1:B, j = 1:B
       if A[i,j]
         push!(graph[i].neighbors, graph[j])
       end
   end
+  graph
 end
 
 #= Groups graph nodes into connected parts. E.g. if entire graph is connected,
   result list will contain only one part with all nodes. =#
-function partition()
+function partition(graph)
   parts = []
   remaining = Set(graph)
   visited = bfs(remaining=remaining)
@@ -123,8 +120,8 @@ end
 
 #= Checks if there's Euler cycle in the graph by investigating
    connectivity condition and evaluating if every vertex has even degree =#
-function check_euler()
-  if length(partition()) == 1
+function check_euler(graph)
+  if length(partition(graph)) == 1
     return all(map(v -> iseven(length(v.neighbors)), graph))
   end
     "Graph is not connected"
@@ -132,7 +129,7 @@ end
 
 #= Returns text representation of the graph consisiting of each node's value
    text and number of its neighbors. =#
-function graph_to_str()
+function graph_to_str(graph)
   graph_str = ""
   for v in graph
     graph_str *= "****\n"
@@ -152,18 +149,22 @@ end
 
 #= Tests graph functions by creating 100 graphs, checking Euler cycle
   and creating text representation. =#
-function test_graph()
-  for i=1:100
-    global graph = GraphVertex[]
+  function test_graph()
+    # Number of graph nodes.
+    N::Int64 = 800
+    # Number of graph edges.
+    K::Int64 = 10000
 
-    A = generate_random_graph()
-    nodes = generate_random_nodes()
-    convert_to_graph(A, nodes)
+    for i=1:100
+      # graph
+      A::BitArray = generate_random_graph(N, K)
+      nodes::Array{NodeType, 1} = generate_random_nodes(N)
+      graph::Array{GraphVertex, 1} = convert_to_graph(A, nodes)
 
-    str = graph_to_str()
-    # println(str)
-    println(check_euler())
+      str = graph_to_str(graph)
+      # println(str)
+      println(check_euler(graph))
+    end
   end
-end
 
-end
+  end
